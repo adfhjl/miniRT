9667a5c25a5439bf937e6059ceedead5201987b6
MyNameIsTrez <sbos@student.codam.nl>
2023-01-19 18:39:51 +0100

    Forgot to commit utils/

---
+++ src/utils/print_error.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   print_error.c                                      :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: sbos <sbos@student.codam.nl>                 +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/01/17 15:13:35 by sbos          #+#    #+#                 */
+/*   Updated: 2023/01/17 15:13:35 by sbos          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minirt.h"
+
+t_status	rt_print_error(t_error ph_error)
+{
+	static char	*error_messages[] = {
+	[ERROR_INVALID_ARGC] = "Invalid argument count",
+	[ERROR_SYSTEM] = "System error",
+	};
+	char		*error_message;
+
+	error_message = error_messages[ph_error];
+	ft_putendl_fd("Error", STDERR_FILENO);
+	ft_putendl_fd(error_message, STDERR_FILENO);
+	return (ERROR);
+}
