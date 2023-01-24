0487f93bbffad16277627c137235eae6e6ea9c35
MyNameIsTrez <sbos@student.codam.nl>
2023-01-24 18:22:10 +0100

    Don't allow +-INFINITY

--- src/init/parse/parse_float.c
+++ src/init/parse/parse_float.c
@@ -17,7 +17,8 @@ t_status	rt_parse_float(char **line_ptr, float *field_ptr)
 	char	*float_end;
 
 	*field_ptr = ft_strtof(*line_ptr, &float_end);
-	if (*line_ptr == float_end)
+	if (*line_ptr == float_end || *field_ptr == INFINITY
+		|| *field_ptr == -INFINITY)
 		return (rt_print_error(ERROR_FAILED_TO_PARSE_FLOAT));
 	(*line_ptr) = float_end;
 	return (OK);
