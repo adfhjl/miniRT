d3b633c0af3d5c78e267144e28c354ce42ee97d2
MyNameIsTrez <sbos@student.codam.nl>
2023-02-06 19:56:21 +0100

    Use Coverage Gutters extension, instead of web

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -2,6 +2,18 @@
 
 make -C /src DEBUG=1 GCOV=1
 
-afl-cov -d /src/afl/afl-output/master/ --coverage-cmd "cat AFL_FILE | /src/miniRT_gcov" --code-dir /src/obj_gcov --live --overwrite --enable-branch-coverage --lcov-web-all --sleep 1 &> /dev/null &
+# afl-cov -d /src/afl/afl-output/master/ --coverage-cmd "cat AFL_FILE | /src/miniRT_gcov" --code-dir /src/obj_gcov --live --overwrite --enable-branch-coverage --lcov-web-all --sleep 1 &> /dev/null &
+
+afl-cov -d /src/afl/afl-output/master/ --coverage-cmd "cat AFL_FILE | /src/miniRT_gcov" --code-dir /src/obj_gcov --live --overwrite --enable-branch-coverage --sleep 1 &> /dev/null &
+
+replace_lcov_docker_work_directory () {
+	while true
+	do
+		< /src/afl/afl-output/master/cov/lcov/trace.lcov_info_final sed 's#/src/src#/Users/sbos/Documents/Programming/Projects/miniRT/src#g' > /src/afl/afl-output/master/cov/lcov/lcov.info
+		sleep 1
+	done
+}
+
+replace_lcov_docker_work_directory &
 
 fuzz.sh
