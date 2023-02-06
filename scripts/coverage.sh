e1b24f93d68f1fae31d5eb4cddd297e1101f4928
MyNameIsTrez <sbos@student.codam.nl>
2023-02-06 22:13:14 +0100

    Add comments

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 export AFL_LLVM_LAF_ALL=1
+
+# These are mutually exclusive
 export AFL_HARDEN=1
+# TODO: Right now this prints "ld.lld: error: cannot open /usr/lib/llvm-14/lib/clang/14.0.6/lib/linux/libclang_rt.asan_static-x86_64.a: No such file or directory"
+# TODO: Might not work well with parallelization "you should only run one afl-fuzz instance per sanitizer type."
+# https://github.com/AFLplusplus/AFLplusplus/blob/stable/docs/fuzzing_in_depth.md#c-selecting-sanitizers
+# export AFL_USE_ASAN=1
 
 make -C /src DEBUG=1 GCOV=1
 
