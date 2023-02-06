9f7be0d0a00aa7ff4caa620298162e5e8501cae2
MyNameIsTrez <sbos@student.codam.nl>
2023-02-06 21:13:59 +0100

    Immediately sleep for a second to prevent error

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -2,15 +2,16 @@
 
 make -C /src DEBUG=1 GCOV=1
 
+# In case you want to view the web result, use this instead of the line below it
 # afl-cov -d /src/afl/afl-output/master/ --coverage-cmd "cat AFL_FILE | /src/miniRT_gcov" --code-dir /src/obj_gcov --live --overwrite --enable-branch-coverage --lcov-web-all --sleep 1 &> /dev/null &
 
-afl-cov -d /src/afl/afl-output/master/ --coverage-cmd "cat AFL_FILE | /src/miniRT_gcov" --code-dir /src/obj_gcov --live --overwrite --enable-branch-coverage --sleep 1 &> /dev/null &
+afl-cov -d /src/afl/afl-output/master/ --coverage-cmd "cat AFL_FILE | /src/miniRT_gcov" --code-dir /src/obj_gcov --live --overwrite --enable-branch-coverage --disable-lcov-web --sleep 1 &> /dev/null &
 
 replace_lcov_docker_work_directory () {
 	while true
 	do
-		< /src/afl/afl-output/master/cov/lcov/trace.lcov_info_final sed 's#/src/src#/Users/sbos/Documents/Programming/Projects/miniRT/src#g' > /src/afl/afl-output/master/cov/lcov/lcov.info
 		sleep 1
+		< /src/afl/afl-output/master/cov/lcov/trace.lcov_info_final sed 's#/src/src#/Users/sbos/Documents/Programming/Projects/miniRT/src#g' > /src/afl/afl-output/master/cov/lcov/lcov.info
 	done
 }
 
