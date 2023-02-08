5d7055e6e8413c6ef69e080c1afad0713e08fca9
MyNameIsTrez <sbos@student.codam.nl>
2023-02-08 15:30:37 +0100

    Add documentation for get_plane_collision_info()

--- src/mathematics/shape_calculations/plane_calculations.c
+++ src/mathematics/shape_calculations/plane_calculations.c
@@ -12,6 +12,30 @@
 
 #include "minirt.h"
 
+// Equation 1:
+// ray.origin + ray.normal * t = rayEnd
+//
+// Equation 2:
+// dot(rayEnd - plane.origin, plane.normal) = 0
+//
+// Substitute rayEnd from equation 1 in equation 2:
+// dot(ray.origin + ray.normal * t - plane.origin, plane.normal) = 0
+//
+// Solve for t:
+// Fact: The dot product is distributive; that is, dot(v1 + v2, v3) = dot(v1, v3) + dot(v2, v3)
+// dot(ray.normal * t, plane.normal) + dot(ray.origin - plane.origin, plane.normal) = 0
+//
+// dot(ray.normal * t, plane.normal) = -dot(ray.origin - plane.origin, plane.normal)
+//
+// Fact: The dot product abides to scalar multiplication; that is, dot(s1 * v1, v2) = s1 * dot(v1, v2)
+// t * dot(ray.normal, plane.normal) = -dot(ray.origin - plane.origin, plane.normal)
+//
+// t = -dot(ray.origin - plane.origin, plane.normal) / dot(ray.normal, plane.normal)
+//
+// t = dot(plane.origin - ray.origin, plane.normal) / dot(ray.normal, plane.normal)
+//
+// denom = dot(ray.normal, plane.normal):
+// t = dot(plane.origin - ray.origin, plane.normal) / denom
 t_hit_info	rt_get_plane_collision_info(
 	t_ray ray, t_object *object, t_data *data)
 {
