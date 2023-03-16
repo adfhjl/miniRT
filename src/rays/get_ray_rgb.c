f4d72c9a817d4d32a5454bb691864d9fb7ccfb69
MyNameIsTrez <sbos@student.codam.nl>
2023-03-16 16:53:34 +0100

    Refactoring rt_get_ray_rgb()

--- src/rays/get_ray_rgb.c
+++ src/rays/get_ray_rgb.c
@@ -27,24 +27,41 @@
 static float	rt_fresnel_reflect_amount(float n1, float n2, t_vector normal,
 					t_vector incident, float f0, float f90)
 {
+	float	r0;
+	float	cos_x;
+	float	n;
+	float	sin_t2;
+	float	x;
+	float	ret;
+
 	// Schlick aproximation.
-	float r0 = (n1-n2) / (n1+n2);
+	r0 = (n1 - n2) / (n1 + n2);
 	r0 *= r0;
-	float cosX = -rt_dot(normal, incident);
+	cos_x = -rt_dot(normal, incident);
 	if (n1 > n2)
 	{
-		float n = n1/n2;
-		float sinT2 = n*n*(1.0f-cosX*cosX);
+		n = n1 / n2;
+		sin_t2 = n * n * (1.0f - cos_x * cos_x);
 		// Total internal reflection.
-		if (sinT2 > 1.0f)
+		if (sin_t2 > 1.0f)
 			return (f90);
-		cosX = sqrtf(1.0f-sinT2);
+		cos_x = sqrtf(1.0f - sin_t2);
 	}
-	float x = 1.0f-cosX;
-	float ret = r0+(1.0f-r0)*x*x*x*x*x;
-
-	// adjust reflect multiplier for object reflectivity.
+	x = 1.0f - cos_x;
+	ret = r0 + (1.0f - r0) * x * x * x * x * x;
+	// Adjust reflect multiplier for object reflectivity.
 	return (rt_lerp(f0, f90, ret));
+}
+
+static bool	rt_russian_roulette(t_vector *throughput)
+{
+	float	p;
+
+	p = rt_max(throughput->r, rt_max(throughput->g, throughput->b));
+	if (rt_random_float_01() > p)
+		return (true);
+	*throughput = rt_scale(*throughput, 1.0f / p);
+	return (false);
 }
 
 // Source:
@@ -52,12 +69,63 @@ static float	rt_fresnel_reflect_amount(float n1, float n2, t_vector normal,
 // casual-shadertoy-path-tracing-1-basic-camera-diffuse-emissive/
 static t_vector	rt_random_unit_vector(void)
 {
-	float z = rt_random_float_01() * 2.0f - 1.0f;
-	float a = rt_random_float_01() * 2.0f * (float)M_PI;
-	float r = sqrtf(1.0f - z * z);
-	float x = r * cosf(a);
-	float y = r * sinf(a);
+	float	z;
+	float	a;
+	float	r;
+	float	x;
+	float	y;
+
+	z = rt_random_float_01() * 2.0f - 1.0f;
+	a = rt_random_float_01() * 2.0f * (float)M_PI;
+	r = sqrtf(1.0f - z * z);
+	x = r * cosf(a);
+	y = r * sinf(a);
 	return ((t_vector){x, y, z});
+}
+
+static bool	rt_total_internal_reflection_occurred(t_vector dir)
+{
+	return (dir.x == 0 && dir.y == 0 && dir.z == 0);
+}
+
+static t_vector	rt_get_refraction_ray_dir(t_hit_info info, t_ray ray,
+					float *do_specular, float *do_refraction)
+{
+	float		eta;
+	t_vector	refraction_ray_dir;
+	float		t;
+	t_vector	diffuse_ray_dir;
+
+	rt_assert_normal(ray.dir, "e");
+	rt_assert_normal(info.surface_normal, "f");
+	eta = 1.0f / info.material.index_of_refraction;
+	if (info.inside)
+		eta = info.material.index_of_refraction;
+	refraction_ray_dir = rt_refract(ray.dir, info.surface_normal, eta);
+	if (rt_total_internal_reflection_occurred(refraction_ray_dir))
+	{
+		*do_specular = 1.0f;
+		*do_refraction = 0.0f;
+	}
+	diffuse_ray_dir = rt_normalized(
+			rt_sub(rt_random_unit_vector(), info.surface_normal));
+	t = info.material.refraction_roughness * info.material.refraction_roughness;
+	return (rt_normalized(rt_mix(refraction_ray_dir, diffuse_ray_dir, t)));
+}
+
+static void	rt_get_diffuse_and_specular_ray_dir(t_vector *diffuse_ray_dir,
+				t_vector *specular_ray_dir, t_hit_info info, t_ray ray)
+{
+	float	t;
+
+	*diffuse_ray_dir = rt_normalized(
+			rt_add(info.surface_normal, rt_random_unit_vector()));
+	*specular_ray_dir = rt_reflect(ray.dir, info.surface_normal);
+	// Note how the specular ray direction doesn't call random().
+	// This means it'll always go the same direction.
+	t = info.material.specular_roughness * info.material.specular_roughness;
+	*specular_ray_dir = rt_normalized(
+			rt_mix(*specular_ray_dir, *diffuse_ray_dir, t));
 }
 
 static void	rt_update_ray_pos(t_ray *ray, t_hit_info info,
@@ -72,7 +140,8 @@ static void	rt_update_ray_pos(t_ray *ray, t_hit_info info,
 	ray->pos = rt_add(ray->pos, rt_scale(info.surface_normal, nudge));
 }
 
-static float	rt_get_ray_action(float *do_specular, float *do_refraction, float specular_chance, float refraction_chance)
+static float	rt_get_ray_action(float *do_specular, float *do_refraction,
+					float specular_chance, float refraction_chance)
 {
 	float	ray_select_roll;
 	float	ray_probability;
@@ -83,7 +152,8 @@ static float	rt_get_ray_action(float *do_specular, float *do_refraction, float s
 		*do_specular = 1.0f;
 		ray_probability = specular_chance;
 	}
-	else if (refraction_chance > 0.0f && ray_select_roll < specular_chance + refraction_chance)
+	else if (refraction_chance > 0.0f
+		&& ray_select_roll < specular_chance + refraction_chance)
 	{
 		*do_refraction = 1.0f;
 		ray_probability = refraction_chance;
@@ -100,6 +170,7 @@ static void	rt_get_specular_and_refraction_chances(float *specular_chance,
 {
 	float	n1;
 	float	n2;
+	float	chance_multiplier;
 
 	if (*specular_chance > 0.0f)
 	{
@@ -110,9 +181,9 @@ static void	rt_get_specular_and_refraction_chances(float *specular_chance,
 		if (info.inside)
 			n2 = 1.0f;
 		*specular_chance = rt_fresnel_reflect_amount(n1, n2, ray.dir,
-			info.surface_normal, info.material.specular_chance, 1.0f);
-		float chance_multiplier;
-		chance_multiplier = (1.0f - *specular_chance) / (1.0f - info.material.specular_chance);
+				info.surface_normal, info.material.specular_chance, 1.0f);
+		chance_multiplier = (1.0f - *specular_chance)
+			/ (1.0f - info.material.specular_chance);
 		*refraction_chance *= chance_multiplier;
 	}
 }
@@ -147,7 +218,8 @@ t_rgb	rt_get_ray_rgb(t_ray ray, t_data *data)
 	size_t		bounce_index;
 
 	rgb = (t_rgb){0, 0, 0};
-	background = rt_srgb_to_linear((t_rgb){BACKGROUND_R, BACKGROUND_G, BACKGROUND_B});
+	background = rt_srgb_to_linear(
+			(t_rgb){BACKGROUND_R, BACKGROUND_G, BACKGROUND_B});
 	throughput = (t_rgb){1, 1, 1};
 
 	bounce_index = 0;
@@ -190,32 +262,18 @@ t_rgb	rt_get_ray_rgb(t_ray ray, t_data *data)
 
 
 
-		// Calculate the diffuse and specular ray directions.
 		t_vector	diffuse_ray_dir;
-		diffuse_ray_dir = rt_normalized(rt_add(info.surface_normal, rt_random_unit_vector()));
 		t_vector	specular_ray_dir;
-		specular_ray_dir = rt_reflect(ray.dir, info.surface_normal);
-		// Note how the specular ray direction doesn't call random().
-		// This means it'll always go the same direction.
-		specular_ray_dir = rt_normalized(rt_mix(specular_ray_dir, diffuse_ray_dir, info.material.specular_roughness * info.material.specular_roughness));
+
+		rt_get_diffuse_and_specular_ray_dir(&diffuse_ray_dir, &specular_ray_dir, info, ray);
+
+
 
 		t_vector	refraction_ray_dir;
-		rt_assert_normal(ray.dir, "e");
-		rt_assert_normal(info.surface_normal, "f");
-		float	eta;
-		eta = 1.0f / info.material.index_of_refraction;
-		if (info.inside)
-			eta = info.material.index_of_refraction;
-		refraction_ray_dir = rt_refract(ray.dir, info.surface_normal, eta);
 
-		// Total internal reflection has occurred.
-		if (refraction_ray_dir.x == 0 && refraction_ray_dir.y == 0 && refraction_ray_dir.z == 0)
-		{
-			do_specular = 1.0f;
-			do_refraction = 0.0f;
-		}
+		refraction_ray_dir = rt_get_refraction_ray_dir(info, ray, &do_specular, &do_refraction);
 
-		refraction_ray_dir = rt_normalized(rt_mix(refraction_ray_dir, rt_normalized(rt_sub(rt_random_unit_vector(), info.surface_normal)), info.material.refraction_roughness * info.material.refraction_roughness));
+
 
 		// The boolean check is what causes specular highlights.
 		ray.dir = rt_mix(diffuse_ray_dir, specular_ray_dir, do_specular);
@@ -223,22 +281,29 @@ t_rgb	rt_get_ray_rgb(t_ray ray, t_data *data)
 		ray.dir = rt_mix(ray.dir, refraction_ray_dir, do_refraction);
 		rt_assert_normal(ray.dir, "i");
 
+
+
 		rgb = rt_add(rgb, rt_multiply_rgb(info.material.emissive, throughput));
+
+
 
 		if (do_refraction == 0.0f)
 			throughput = rt_multiply_rgb(throughput, rt_mix(info.material.rgb, (t_rgb){1, 1, 1}, do_specular));
 
-		// since we chose randomly between diffuse and specular,
+		// Since we chose randomly between diffuse and specular,
 		// we need to account for the times we didn't do one or the other.
 		throughput = rt_scale(throughput, 1.0f / ray_probability);
 
-		// Russian roulette.
-		float p = rt_max(throughput.r, rt_max(throughput.g, throughput.b));
-		if (rt_random_float_01() > p)
+
+
+		if (rt_russian_roulette(&throughput))
 			break ;
-		throughput = rt_scale(throughput, 1.0f / p);
+
+
 
 		rgb = rt_add(rgb, rt_multiply_rgb(rt_scale(data->ambient->material.rgb, data->ambient->ratio), throughput));
+
+
 
 		bounce_index++;
 	}
